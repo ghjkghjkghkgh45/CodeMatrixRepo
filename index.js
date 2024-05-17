e2ae17beb216b61e60d01aae0055051013ae4c0e
ghjkghjkghkgh45 <js1kamilyvk@hotmail.com>
2024-05-18 04:44:19 +0700

    commit

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-const randomNumber = (min, max) =>
-  Math.floor(Math.random() * (max - min + 1) + min);
+function countBits(num) {
+  const result = new Array(num + 1).fill(0);
+  for (let i = 1; i <= num; i++) {
+    result[i] = result[i >> 1] + (i & 1);
+  }
+  return result;
+}
